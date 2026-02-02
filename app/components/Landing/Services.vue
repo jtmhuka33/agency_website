df578300f45c4c697e272523f69ca65b91921d04
jtmhuka33 <jtmhuka33@gmail.com>
2026-02-02 18:59:52 +0100

    feat: complete services section

--- app/components/Landing/Services.vue
+++ app/components/Landing/Services.vue
@@ -1,35 +1,31 @@
 <script setup lang="ts">
 const cards = useState(() => [
   {
-    title: 'Web Development',
-    description: 'High-performance, responsive websites using modern technologies.',
-    icon: 'i-lucide-smile',
+    title: "Web Development",
+    description:
+      "High-performance, responsive websites using modern technologies.",
+    icon: "i-lucide-smile",
   },
   {
-    title: 'Custom Software',
-    description: 'Internal tools and dashboards to automate workflows.',
-    icon: 'i-lucide-a-large-small',
+    title: "AI Integration",
+    description: "The best flagship models at your fingertips",
+    icon: "i-lucide-a-large-small",
   },
   {
-    title: 'Mobile App Development',
-    description: 'Responsive apps that work on any device.',
-    icon: 'i-lucide-sun-moon',
+    title: "Mobile App Development",
+    description:
+      "iOS and Android app development reaching your customers on any device",
+    icon: "i-lucide-sun-moon",
   },
-])
+]);
 </script>
 
 <template>
   <UContainer>
-    <UPageGrid>
-      <UPageCard
-        v-for="(card, index) in cards"
-        :key="index"
-        v-bind="card"
-      />
+    <UPageGrid :ui="{ wrapper: 'sm:grid-cols-1' }">
+      <UPageCard v-for="(card, index) in cards" :key="index" v-bind="card" />
     </UPageGrid>
   </UContainer>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
